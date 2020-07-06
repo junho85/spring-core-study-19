be5f3f05f00220f16aaacd6cb4292c920804b43f
june.kim <junho85@gmail.com>
2020-07-06 23:30:46 +0900

    20.스프링 AOP: @AOP

---
+++ src/main/java/kr/pe/junho85/demo/PerfAspect.java
@@ -0,0 +1,26 @@
+package kr.pe.junho85.demo;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerfAspect {
+//    @Around("execution(* kr.pe.junho85..*.EventService.*(..))")
+//    @Around("@annotation(PerfLogging)")
+    @Around("bean(simpleEventService)")
+    public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
+        long begin = System.currentTimeMillis();
+        Object retVal = pjp.proceed();
+        System.out.println(System.currentTimeMillis() - begin);
+        return retVal;
+    }
+
+    @Before("bean(simpleEventService)")
+    public void hello() {
+        System.out.println("hello");
+    }
+}
